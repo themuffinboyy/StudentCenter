0147b2780bd274d9b463fbea29145a5dbb7947c4
themuffinboyy <hollyj71@students.rowan.edu>
2015-08-20 11:35:53 -0400

    marketing updates

--- marketing.php
+++ marketing.php
@@ -35,31 +35,165 @@
       <li class="accordion-navigation">
         <a href="#panel1a">General Student Center Posting Guidelines</a>
         <div id="panel1a" class="content">
+          <ol>
+            <li>Posting may occur on designated bulletin boards and posting strips only, not on walls, doors or
+            columns on the interior or exterior of the building. Posted materials must be fastened using staples
+            or thumbtacks only.</li>
+            <li>All outdoor posting is prohibited. Specifically, posting is prohibited on trees, light posts,
+            sidewalks, trash receptacles, benches, etc. Exception: banner posting is limited to one banner per
+            event per organization. The defined area for banner posting is behind the Student Center (wooden
+            fence between Student Center building and wooden bridge) and the fence around the outdoor
+            basketball courts.</li>
+            <li>Chalking on the patios or walkways of the Student Center is prohibited.</li>
+            <li>Exceptions to any posting policy listed above must be proposed in writing and approved by the
+            administrative staff of the Student Center.</li>
+            <li>Violations of any of the above policies may result in sanctions.</li>
+          </ol>
+
+          <strong>Pre-Approval</strong>
+          <ol>
+            <li>All materials advertising on-campus events must include the name of the sponsoring organization,
+            group or department, event start and end time.</li>
+            <li>For all student organizations, pre-approval must be obtained from the Student Center (approval/date
+            stamp on flyer, poster or banner). The original must be approved and stamped PRIOR TO
+            PRINTING. Student Center staff will post within one business day.</li>
+            <li>Flier size for bulletin boards is limited to 81/2 by 11" or 81/2 by 14" for all postings. Exceptions:
+            university sponsored or external events that have pre-printed posters size 11 by 17".</li>
+            <li>Postings not approved by the appropriate University personnel will be removed immediately.</li>
+          </ol>
+
+          <strong>Posting Procedures</strong>
+          <ol>
+            <li>Posting on designated bulletin boards is limited to 1 flier per event per board and only one single
+            layer of fliers allowed (no overlapping).</li>
+            <li>Postings may go up no more than 2 weeks prior to the event and will be removed by the next
+            business day following the completion of the event.</li>
+            <li>Posted materials must be fastened using staples or postings only (no tape or other adhesives).</li>
+            <li>Posting of fliers/posters/banners is not permitted on windows, window treatments, painted surfaces,
+            walls, furniture, brick, concrete, wood surfaces, buildings, light poles, trees or campus signage.</li>
+            <li>Posting by external groups (outside of the campus community), vendors and miscellaneous
+            individual postings (i.e. "books for sale", "roommate needed") are permitted on the 2nd floor of the
+            Student Center.</li>
+            <li>Directional signs must be staked and removed by the next business day after completion of the
+            event.</li>
+            <li>In the event of “Flier overload” (more fliers or posters than the bulletin board space available), the
+            following set of posting priorities may be applied in date order:
+            <ul>
+              • Rowan University student clubs/organizations and/or academic and administrative
+              departments
+              • Individuals affiliated with Rowan University
+              • All others
+            </ul>
+            </li>
+          </ol>
         </div>
       </li>
       <li class="accordion-navigation">
         <a href="#panel2a">Bulletin Boards</a>
         <div id="panel2a" class="content">
+          <ol>
+            <li>Encased bulletin boards are located on the ground floor;</li>
+            <li>Due to limited space, postings will be available on a first-come, first serve basis.</li>
+            <li>Only one flier per group per event can be posted.</li>
+            <li>Fliers can only be posted for a maximum of fourteen (14) days.</li>
+            <li>Flier size for bulletin boards is limited to 81/2 by 11" or 81/2 by 14" for all postings. Exceptions:
+            University/Dept. sponsored events that have pre-printed posters 11” by 17".</li>
+          </ol>
+          <strong>Bulletin Board Posting and Approval Procedures</strong>
+          <ol>
+            <li>Complete a “Posting Request Form” along with a copy of the flier and submit both of them to the
+            Student Center Information Desk or Administrative Office.</li>
+            <li>The flier will be reviewed for approval by the next business day.
+            Fliers for encased bulletin boards can only be approved by the Student Center staff.</li>
+            <li>The SC staff will post all materials according to completed request form.</li>
+          </ol>
         </div>
       </li>
       <li class="accordion-navigation">
         <a href="#panel3a">Posting Strips</a>
         <div id="panel3a" class="content">
+          <ol>
+            <li>Posting strips are located along the corridor on the main floor and 2nd floor.</li>
+            <li>Student organizations may post one flier per event on each floor. Multiple postings will be
+            removed if space is limited.</li>
+            <li>External groups (outside of the campus community, i.e. vendors) and individual advertisements (i.e.
+            “roommate needed”) can only be posted on the second floor of the Student Center.</li>
+            <li>Postings are limited to a size of 81/2 by 11" or 81/2 by 14" for all postings.</li>
+          </ol>
+
+          <strong>Posting Strips Approval Process</strong>
+          <ol>
+            <li>For all chartered internal and external organizations, pre-approval must be obtained by the Student
+            Center administrative staff prior to posting (in the form of a date/dept stamp).</li>
+            <li>Fliers can only be posted for a maximum of fourteen (14) days.</li>
+          </ol>
         </div>
       </li>
       <li class="accordion-navigation">
         <a href="#panel4a">Digital Signage</a>
         <div id="panel4a" class="content">
+          <ol>
+            <li>Only registered student organizations, and campus departments/offices, are permitted to display
+            content on the Digital Signage System. Use of the Digital Signage System is at no cost. Email
+            studentcenter@rowan.edu to register for an account.</li>
+            <li>The creation of content for display on the Digital Signage System is the responsibility of the group
+            presenting the information to the campus community.</li>
+            <li>Content must be submitted electronically a minimum of five (5) business days prior to the first date
+            the content is to be displayed on the Digital Signage system.</li>
+            <li>Announcements will run for a maximum of 14 days from the start date.</li>
+            <li>Announcements will run for a maximum of 15 seconds at a time.</li>
+            <li>Each organization may post one (1) advertisement per event on the Digital Signage display.</li>
+            <li>The Student Center reserves the right to reject any material that includes references to the sale or
+            consumption of alcohol or other drugs or that promotes/condones behavior that violates University
+            policies. Any violation of local, state or federal law will also not be approved for display.</li>
+            <li>Advertisement of commercial products or services on the Digital Signage System is prohibited.</li>
+            <li>Content that infringes on the copyrighted or trademarked works of others will not be approved for
+            display. Copyrighted and trademarked material may include, but are not limited to: logos, digital
+            images, photographs, paintings, movies, videos, and written works.</li>
+            <li>To optimize the management of Digital Signage content, The Student Center reserves the right to
+            determine what content is displayed at which Digital Signage location(s) and determine the duration,
+            dates, and times content is displayed.</li>
+          </ol>
         </div>
       </li>
       <li class="accordion-navigation">
         <a href="#panel5a">Banners/Posters</a>
         <div id="panel5a" class="content">
+          <ol>
+            <li>Any banners or posters placed in the Pit Lounge area must be cleared by the Student Center
+            Director or his/her designee.</li>
+            <li>Banners may only be for specific events and special announcements subject to review by the
+            Student Center Administration.</li>
+            <li>Student Organizations chartered by SGA may hang a banner in the Pit Area. The banner cannot
+            exceed a length of 6 feet by 41/2 feet in width, with a minimum size of 21/2 feet by 2 feet.</li>
+            <li>Each organization may only hang ONE banner, per event, at any given time. The banner may not be
+            hung for more than fourteen days.</li>
+            <li>Space is on a first come, first serve basis. Banners may only be moved by the sponsoring group or
+            Student Center staff.</li>
+            <li>Banners may not advertise general meetings for organizations.</li>
+            <li>Banners must state the sponsoring organization of the event. SGA must sponsor any non-chartered
+            group that wants to post information in the Pit.</li>
+            <li>Fliers, table tents or sheets may not be posted in the Pit.</li>
+            <li>Banners must be removed by sponsoring group when the event is over. The Student Center staff
+            reserves the right to remove banners if guidelines are not followed.</li>
+            <li>The Student Center reserves the right to remove banners temporarily for university wide events
+            such as Homecoming.</li>
+            <li>Posters or banners must be hung with string or yarn, no tape is permitted. Posters or banners will be
+            removed if not posted correctly.</li>
+            <li>Posters or banners with glitter are not permitted.</li>
+          </ol>
         </div>
       </li>
       <li class="accordion-navigation">
         <a href="#panel6a">Free Speech and Peaceful Assembly Policy</a>
         <div id="panel6a" class="content">
+          <p>The entire Free Speech and Peaceful Assembly policy can be found in the Rowan Student Handbook and
+          Employee handbook. The full policy and demonstration request form can be found at
+          www.rowan.edu/freespeechpolicy. This policy complies with all applicable federal constitutional and State
+          laws. Please refer to this policy for additional details.</p>
+
+          <p>Note: The Chamberlain Student Center staff can assist you with planning a successful demonstration or
+          responsive event. Email studentcenter@rowan.edu to request an event consultation meeting.</p>
         </div>
       </li>
       <li class="accordion-navigation">
