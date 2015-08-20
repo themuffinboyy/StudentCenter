93ab5e367b38e7a7bdbce753b1c4b8467a368762
themuffinboyy <hollyj71@students.rowan.edu>
2015-08-20 11:10:02 -0400

    starting marketing

--- marketing.php
+++ marketing.php
@@ -1,102 +1,79 @@
 <!doctype html>
 <html class="no-js" lang="en">
-  <head>
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" href="images/csc_favicon.png" type="image/png" sizes="16x16">
-      <title>Chamberlain Student Center | Rowan University</title>
-      <link rel="stylesheet" href="css/foundation.css" />
-      <link rel="stylesheet" href="css/marketing.css" />
-      <link rel="stylesheet" href="css/banner.css" />
-      <link rel="stylesheet" href="css/rowanheader.css" />
-      <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.js"></script>
-      <script type="text/javascript" src="js/js_jnv80YOwCwlPiNZ0W1fOaWsUliw1fl3r-iYl0nsElnY.js"></script>
-      <script type="text/javascript" src="js/js_jckA-4t9p22mBBR89owx6Ksvb40v5gsspPqsDK3Q0oM.js"></script>
-      <script type="text/javascript" src="js/js_-WcrGf3pH5GnSWA1Y-B28tyjknNjFr4asJklCZQ5NUA.js"></script>
-      <script type="text/javascript" src="js/js_6tUAv_hfkMxyYfPGNFOzEz92zxJy6vNy2_-n8RWvupM.js"></script>
-      <script type="text/javascript" src="js/js_IDBX5SzkJ9gGNq7x-qOE_2DZsexqguTJQGMKvi4w-Uw.js"></script>
-      <script type="text/javascript" src="js/js_q7cpEY7s0Al6EkhNnFajyKCyiVdTKqAcAvZS1arQ-D8.js"></script>
-      <script type="text/javascript" src="js/js_lpa3ttN1nRqoLlwqFJjkpfFvmjM6Z09FecPa_kywAeM.js"></script>
-      <script src="js/vendor/jquery.js"></script>
-      <script src="js/vendor/modernizr.js"></script>
-      <script src="js/foundation/foundation.js"></script>
-      <script src="js/foundation/foundation.orbit.js"></script>
-      <script src="js/foundation/foundation.accordion.js"></script>
-      <script src="js/foundation/foundation.tab.js"></script>
-      <script src="js/foundation/foundation.topbar.js"></script>
-  </head>
-  <body>
-    <?php include("header.html"); ?>
-
-    <section id="marketing_page">
-      <div class="row marketing_header">
-        <div class="large-12 columns">
-          <img src="images/marketing_header.jpg">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="icon" href="images/csc_favicon.png" type="image/png" sizes="16x16">
+<title>Chamberlain Student Center | Rowan University</title>
+<link rel="stylesheet" href="css/foundation.css" />
+<link rel="stylesheet" href="css/banner.css" />
+<link rel="stylesheet" href="css/rowanheader.css" />
+<link rel="stylesheet" href="css/marketing.css" />
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.js"></script>
+<script type="text/javascript" src="js/js_jnv80YOwCwlPiNZ0W1fOaWsUliw1fl3r-iYl0nsElnY.js"></script>
+<script type="text/javascript" src="js/js_jckA-4t9p22mBBR89owx6Ksvb40v5gsspPqsDK3Q0oM.js"></script>
+<script type="text/javascript" src="js/js_-WcrGf3pH5GnSWA1Y-B28tyjknNjFr4asJklCZQ5NUA.js"></script>
+<script type="text/javascript" src="js/js_6tUAv_hfkMxyYfPGNFOzEz92zxJy6vNy2_-n8RWvupM.js"></script>
+<script type="text/javascript" src="js/js_IDBX5SzkJ9gGNq7x-qOE_2DZsexqguTJQGMKvi4w-Uw.js"></script>
+<script type="text/javascript" src="js/js_q7cpEY7s0Al6EkhNnFajyKCyiVdTKqAcAvZS1arQ-D8.js"></script>
+<script type="text/javascript" src="js/js_lpa3ttN1nRqoLlwqFJjkpfFvmjM6Z09FecPa_kywAeM.js"></script>
+<script src="js/vendor/modernizr.js"></script>
+<script src="js/vendor/jquery.js"></script>
+<script src="js/foundation/foundation.js"></script>
+<script src="js/foundation/foundation.topbar.js"></script>
+<script src="js/foundation/foundation.accordion.js"></script>
+</head>
+<body>
+<?php 
+  include("header.html");
+?>
+<section id="marketing_page">
+<div class="large-9 large-centered column">
+  <div class="panel">
+    <h1 id="top-bar">Marketing</h1><br>
+    <ul class="accordion" data-accordion>
+      <li class="accordion-navigation">
+        <a href="#panel1a">General Student Center Posting Guidelines</a>
+        <div id="panel1a" class="content">
         </div>
-      </div>
-
-      <div class="row">
-        <div class="large-4 columns">
-          <div class="label">Digital Signage</div>
-
-          <div class="panel">
-            <p>All officially recognized student organizations and University departments & offices are permitted to display
-            content on the Digital Signage System.</p>
-
-            <ul class="button-group">
-              <li><a href="#" class="button">Register</a></li>
-              <li><a href="#" class="button">Login</a></li>
-            </ul>
-
-            <ul class="button-group">
-              <li><a href="#" class="button">Policies</a></li>
-              <li><a href="#" class="button">Help</a></li>
-            </ul>
-          </div>
+      </li>
+      <li class="accordion-navigation">
+        <a href="#panel2a">Bulletin Boards</a>
+        <div id="panel2a" class="content">
         </div>
-
-        <div class="large-4 columns">
-          <div class="label">Bulletin Board</div>
-
-          <div class="panel">
-            <p>Bunch of bulletin board text.</p>
-          </div>
+      </li>
+      <li class="accordion-navigation">
+        <a href="#panel3a">Posting Strips</a>
+        <div id="panel3a" class="content">
         </div>
-
-        <div class="large-4 columns">
-          <div class="label">Posting Strips</div>
-
-          <div class="panel">
-            <p>Bunch of posting strips text.</p>
-          </div>
+      </li>
+      <li class="accordion-navigation">
+        <a href="#panel4a">Digital Signage</a>
+        <div id="panel4a" class="content">
         </div>
-      </div>
-
-      <div class="row">
-        <div class="large-4 columns">
-          <div class="label">Banner</div>
-
-          <div class="panel">
-            <p>Bunch of banner text.</p>
-          </div>
+      </li>
+      <li class="accordion-navigation">
+        <a href="#panel5a">Banners/Posters</a>
+        <div id="panel5a" class="content">
         </div>
-
-        <div class="large-4 columns">
-          <div class="label">Signage in the Building</div>
-          <div class="panel">
-            <p>Bunch of signage in the building text.</p>
-          </div>
+      </li>
+      <li class="accordion-navigation">
+        <a href="#panel6a">Free Speech and Peaceful Assembly Policy</a>
+        <div id="panel6a" class="content">
         </div>
-
-        <div class="large-4 columns">
-          <div class="label">FAQS</div>
-          <div class="panel">
-            <p>Bunch of questions that are asked frequently.</p>
-          </div>
+      </li>
+      <li class="accordion-navigation">
+        <a href="#panel7a">FAQs</a>
+        <div id="panel7a" class="content">
         </div>
-      </div>
+      </li>
+    </ul>
+  </div>
+</div>
+</section>
+<?php 
+  include("footer.html");
+?>
 
-      <?php include("footer.html"); ?>
-    </section>    
-  </body>
+</body>
 </html>
